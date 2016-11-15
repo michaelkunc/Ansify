4a8dd4fe3ce4861962d675b4743d8121ac5d3270
Michael Kunc <mjkunc@gmail.com>
2016-11-15 15:11:08 -0800

    another line of tests passing

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -81,11 +81,11 @@ class SQLParserTest(unittest.TestCase):
         statement = ("SELECT DISTINCT \n"
                      "RCTLGDA.CUSTOMER_TRX_ID AS CUSTOMER_TRX_ID, \n"
                      "PEIA.EXPENDITURE_ITEM_ID AS PA_TRANS_ID\n\n"
-                     "TABLE_1\n")
-                     # "INNER JOIN\n"
+                     "TABLE_1\n"
+                     "INNER JOIN\n")
                      # "TABLE_2 ON RCTLGDA.EXPENDITURE_ITEM_ID = PEIA.EXPENDITURE_ITEM_ID\n"
                      # "WHERE RCTLGDA.TRANSACTION_TYPE = 'CAPITAL")
-        self.assertEqual(statement, test_ins.build_select_from()[0:111])
+        self.assertEqual(statement, test_ins.build_select_from()[0:122])
 
 
     def test_build_where(self):
